10992e8252d5c7a2c5024ddaa7bf2ce6dd3d0106
Arbdoul <salisuabdullahi64@gmail.com>
2024-06-01 11:55:12 +0100

    added toggle button

--- components/BottomSheet.tsx
+++ components/BottomSheet.tsx
@@ -1,11 +1,16 @@
-import { BottomSheetBackdrop, BottomSheetModal } from "@gorhom/bottom-sheet";
+import Colors from "@/constants/Colors";
+import {
+  BottomSheetBackdrop,
+  BottomSheetModal,
+  useBottomSheetModal,
+} from "@gorhom/bottom-sheet";
 import React, { forwardRef, useCallback, useMemo } from "react";
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export type Ref = BottomSheetModal;
 const BottomSheet = forwardRef<Ref>((props, ref) => {
   const snapPoints = useMemo(() => ["50%"], []);
-
+  const { dismiss } = useBottomSheetModal();
   const renderBackDrop = useCallback(
     (props: any) => (
       <BottomSheetBackdrop
@@ -18,16 +23,61 @@ const BottomSheet = forwardRef<Ref>((props, ref) => {
   );
   return (
     <BottomSheetModal
+      backgroundStyle={{ backgroundColor: Colors.lightGrey }}
+      handleIndicatorStyle={{ display: "none" }}
       ref={ref}
       overDragResistanceFactor={0}
       snapPoints={snapPoints}
       backdropComponent={renderBackDrop}
     >
-      <Text>BottomSheet</Text>
+      <View style={styles.contentContainer}>
+        <View style={styles.toggle}>
+          <TouchableOpacity
+            style={styles.toggleActive}
+            onPress={() => dismiss()}
+          >
+            <Text>Delivery</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.toggleInactive}
+            onPress={() => dismiss()}
+          >
+            <Text>Pick up</Text>
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity style={styles.button} onPress={() => dismiss()}>
+          <Text>Confirm</Text>
+        </TouchableOpacity>
+      </View>
     </BottomSheetModal>
   );
 });
 
 export default BottomSheet;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  contentContainer: {
+    flex: 1,
+  },
+  toggle: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 20,
+  },
+  toggleActive: {
+    backgroundColor: Colors.primary,
+  },
+  toggleInactive: {},
+  button: {
+    backgroundColor: Colors.primary,
+    padding: 16,
+    margin: 16,
+    borderRadius: 4,
+    alignItems: "center",
+  },
+  btnText: {
+    color: "#fff",
+    fontWeight: "bold",
+  },
+});
