deda69a889022546e2d353207785461813785940
Arbdoul <salisuabdullahi64@gmail.com>
2024-06-03 18:11:06 +0100

    added filter screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,24 @@
 import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
+import { Stack, useNavigation } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import "react-native-reanimated";
 
 import { CustomHeader } from "@/components/custom-header";
+import Colors from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
+import { Ionicons } from "@expo/vector-icons";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 import { StatusBar } from "expo-status-bar";
+import { Platform, TouchableOpacity } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
-
+const isAndroid = Platform.OS === "android";
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const navigation = useNavigation();
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
@@ -41,9 +45,20 @@ export default function RootLayout() {
           <Stack.Screen
             name="(modal)/filter"
             options={{
-              presentation: "modal",
+              presentation: Platform.OS === "android" ? "modal" : "card",
               headerTitle: "Filter",
               headerShadowVisible: false,
+              headerStyle: { backgroundColor: Colors.lightGrey },
+              headerTitleAlign: "center",
+              headerLeft: () => (
+                <TouchableOpacity onPress={() => navigation.goBack()}>
+                  <Ionicons
+                    name="close-outline"
+                    size={26}
+                    color={Colors.primary}
+                  />
+                </TouchableOpacity>
+              ),
             }}
           />
         </Stack>
