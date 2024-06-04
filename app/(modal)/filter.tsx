665e8248d5ed9b352cb8a8b43ba295e8e88f8020
Arbdoul <salisuabdullahi64@gmail.com>
2024-06-04 14:20:53 +0100

    added footer to the modal

--- app/(modal)/filter.tsx
+++ app/(modal)/filter.tsx
@@ -1,14 +1,65 @@
+import Colors from "@/constants/Colors";
+import { useNavigation } from "expo-router";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 const Filter = () => {
+  const navigation = useNavigation();
   return (
-    <View>
+    <View style={styles.container}>
       <Text>filter</Text>
+      <View style={styles.footer}>
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          style={styles.fullButton}
+        >
+          <Text style={styles.footerText}>Done</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
 export default Filter;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: Colors.lightGrey,
+  },
+  footer: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: 100,
+    padding: 10,
+    backgroundColor: "#fff",
+    elevation: 10,
+    shadowOpacity: 0.1,
+    shadowRadius: 10,
+    shadowOffset: {
+      width: 0,
+      height: -10,
+    },
+  },
+  fullButton: {
+    backgroundColor: Colors.primary,
+    padding: 16,
+    alignItems: "center",
+    borderRadius: 8,
+    // elevation: 10,
+    // shadowOpacity: 0.1,
+    // shadowRadius: 10,
+    // shadowOffset: {
+    //   width: 0,
+    //   height: -10,
+    // },
+  },
+  footerText: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+});
