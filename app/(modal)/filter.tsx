a6f453d628fbfe9386ef3bba8d5cea8e4422b40b
Arbdoul <salisuabdullahi64@gmail.com>
2024-06-05 10:52:47 +0100

    added bouncy chexk box

--- app/(modal)/filter.tsx
+++ app/(modal)/filter.tsx
@@ -1,13 +1,74 @@
+import { categories } from "@/assets/data/home";
 import Colors from "@/constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
 import { useNavigation } from "expo-router";
 import React from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  FlatList,
+  ListRenderItem,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import BouncyCheckbox from "react-native-bouncy-checkbox";
+
+interface Category {
+  name: string;
+  count: number;
+  // checked?: boolean;
+  // image: ImageProps;
+  //   text: string;
+  //   img: ImageProps;
+}
+
+const ItemBox = () => (
+  <>
+    <View style={styles.itemContainer}>
+      <TouchableOpacity style={styles.item}>
+        <Ionicons name="arrow-down-outline" size={20} color={Colors.medium} />
+        <Text style={{ flex: 1 }}>Sort</Text>
+        <Ionicons name="chevron-forward" size={20} color={Colors.medium} />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.item}>
+        <Ionicons name="fast-food-outline" size={20} color={Colors.medium} />
+        <Text style={{ flex: 1 }}>Hygiene rating</Text>
+        <Ionicons name="chevron-forward" size={20} color={Colors.medium} />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.item}>
+        <Ionicons name="pricetag-outline" size={20} color={Colors.medium} />
+        <Text style={{ flex: 1 }}>Offers </Text>
+        <Ionicons name="chevron-forward" size={20} color={Colors.medium} />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.item}>
+        <Ionicons name="nutrition-outline" size={20} color={Colors.medium} />
+        <Text style={{ flex: 1 }}>Dietary</Text>
+        <Ionicons name="chevron-forward" size={20} color={Colors.medium} />
+      </TouchableOpacity>
+    </View>
+    <Text style={styles.header}>Categories</Text>
+  </>
+);
 
 const Filter = () => {
   const navigation = useNavigation();
+
+  const renderItem: ListRenderItem<Category> = ({ item }) => (
+    <View style={styles.row}>
+      <Text>
+        {item.name} ({item.count})
+      </Text>
+      <BouncyCheckbox />
+    </View>
+  );
   return (
     <View style={styles.container}>
-      <Text>filter</Text>
+      <FlatList
+        data={categories}
+        renderItem={renderItem}
+        ListHeaderComponent={<ItemBox />}
+      />
+      <View style={{ height: 80 }} />
       <View style={styles.footer}>
         <TouchableOpacity
           onPress={() => navigation.goBack()}
@@ -61,5 +122,34 @@ const styles = StyleSheet.create({
     color: "#fff",
     fontWeight: "bold",
     fontSize: 16,
+  },
+  itemContainer: {
+    backgroundColor: "#fff",
+    borderRadius: 8,
+    padding: 8,
+    marginBottom: 16,
+  },
+  header: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginBottom: 16,
+  },
+  item: {
+    flexDirection: "row",
+    gap: 20,
+    alignItems: "center",
+    backgroundColor: "#fff",
+    paddingVertical: 10,
+    borderColor: Colors.grey,
+    borderBottomWidth: 1,
+  },
+  itemText: {
+    flex: 1,
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 10,
+    backgroundColor: "#fff",
   },
 });
